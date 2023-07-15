4bf17999615c0b430034e026366c80518f84f65d
Sujoy Kumar Roy <sujoykroy@gmail.com>
2023-07-15 17:51:30 +0530

    blank app

---
+++ README.md
@@ -0,0 +1,2 @@
+# picscript
+
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+    <header>
+        <div class="wrapper">
+        </div>
+    </header>
+
+    <main>
+        This is picscript
+    </main>
+</template>
+
+<style scoped>
+
+</style>
