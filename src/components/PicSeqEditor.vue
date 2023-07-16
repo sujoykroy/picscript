7fe8bfbaef287113ffa93765719968848c683825
Sujoy Kumar Roy <sujoykroy@gmail.com>
2023-07-16 11:01:36 +0530

    caret positioning fix

--- src/components/PicSeqEditor.vue
+++ src/components/PicSeqEditor.vue
@@ -5,35 +5,50 @@ import PicBlock from '@/components/PicBlock.vue'
 
 const textList = ref(['A', 'B'])
 const currentText = ref('')
+const lastKey = ref('')
 const currentPicBlockElem = ref()
 
+function placeCaretAtEnd(el) {
+    el.focus()
+    let range = document.createRange()
+    range.selectNodeContents(el)
+    range.collapse(false)
+    var sel = window.getSelection()
+    sel.removeAllRanges()
+    sel.addRange(range)
+}
+
 function onKeyPress(event) {
+    lastKey.value = event.key
+    placeCaretAtEnd(currentPicBlockElem.value.inputElem)
     if (event.key.trim().length && event.key != 'Enter') return
     let newText = currentText.value.trim()
     if (newText) {
         textList.value.push(newText)
     }
-    console.log(newText)
     currentText.value = ''
 }
 </script>
 
 <template>
-    <div class="picblock-container" @click="currentPicBlockElem.inputElem.focus()">
-        <PicBlock
-            contenteditable="false"
-            v-for="(text, textI) of textList"
-            v-bind="textI"
-            :text="text"
-        />
-        <PicBlock
-            class="active-pickblock"
-            ref="currentPicBlockElem"
-            autofocus
-            @onKeyPress="onKeyPress"
-            contenteditable="true"
-            v-model:text="currentText"
-        />
+    <div>
+        <div class="picblock-container" @click="currentPicBlockElem.inputElem.focus()">
+            <PicBlock
+                contenteditable="false"
+                v-for="(text, textI) of textList"
+                v-bind="textI"
+                :text="text"
+            />
+            <PicBlock
+                class="active-pickblock"
+                ref="currentPicBlockElem"
+                autofocus
+                @onKeyPress="onKeyPress"
+                contenteditable="true"
+                v-model:text="currentText"
+            />
+        </div>
+        <div>{{ lastKey.length }}</div>
     </div>
 </template>
 
